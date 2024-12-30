6b4bbf6b16dbfe8ec42095501474c7b7139c0f46
guzen <guzenko.dmitriy@gmail.com>
2024-12-30 09:08:45 +0200

    Hello world

---
+++ src/main/java/com/guzenko/springcourse/TestApplication.java
@@ -0,0 +1,9 @@
+package com.guzenko.springcourse;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class TestApplication {
+    public static void main(String[] args) {
+
+    }
+}
