0f4e881cf55a7ad39010085f99f3e2fbc6e25ad9
guzen <guzenko.dmitriy@gmail.com>
2025-01-03 08:32:28 +0200

    GetMapping

--- src/main/java/com/guzenko/springcourse/TestApplication.java
+++
@@ -1,9 +0,0 @@
-package com.guzenko.springcourse;
-
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-public class TestApplication {
-    public static void main(String[] args) {
-
-    }
-}
