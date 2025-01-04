f5fd2ecc06ed0907d479d85ed22af0d985f31db4
guzen <guzenko.dmitriy@gmail.com>
2025-01-04 12:59:20 +0200

    calculator

--- src/main/java/com/guzenko/springcourse/controllers/FirstController.java
+++ src/main/java/com/guzenko/springcourse/controllers/FirstController.java
@@ -1,20 +1,58 @@
 package com.guzenko.springcourse.controllers;
 
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage() {
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+
+        model.addAttribute("message", "name: " + name + ", surname: " + surname);
         return "first/hello";
     }
     @GetMapping("/goodbye")
     public String goodByePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam(value = "a") int a,
+                          @RequestParam(value = "b") int b,
+                          @RequestParam(value = "action") String action,
+                          Model model) {
+        double result = 0;
+
+        switch (action) {
+            case "multiplication":
+                result = a * b;
+                break;
+            case "division":
+                result = a / (double) b;
+                break;
+            case "addition":
+                result = a + b;
+                break;
+            case "subtraction":
+                result = a - b;
+                break;
+            default:
+                result = 0;
+                break;
+        }
+
+
+        model.addAttribute("result", result);
+        System.out.println(result);
+        return "first/calculator";
     }
 
 }
